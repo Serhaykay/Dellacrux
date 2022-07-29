4cb1a94b096b03cc694c2285745db96497375ae8
Serhaykay <92628969+Serhaykay@users.noreply.github.com>
2022-07-29 11:33:21 +0100

    Add files via upload

--- cart.js
+++ cart.js
@@ -1,34 +1,69 @@
-if (document.readyState == 'loading'){
+if (document.readyState == 'loading') {
     document.addEventListener('DOMContentLoaded', ready)
-} else{
+} else {
     ready()
 }
-function ready(){
+function ready() {
     var removeCartItemButton = document.getElementsByClassName('remove')
-     for (var i = 0; i < removeCartItemButton.length; i++){
-        button = removeCartItemButton[i]
+    for (var i = 0; i < removeCartItemButton.length; i++) {
+        var button = removeCartItemButton[i]
         button.addEventListener('click', removeCartItems
         )
     }
+    var quantityInputs = document.getElementsByClassName('cart-quantity')
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i]
+        input.addEventListener('change', quantityChanged)
+    }
+    var addToCartButtons = document.getElementsByClassName('add-to-cart')
+    for (var i = 0; i < addToCartButtons.length; i++) {
+        var button = addToCartButtons[i]
+        button.addEventListener('click', addToCartClicked)
+    }
 }
-function removeCartItems(event){
+function removeCartItems(event) {
     var buttonClicked = event.target
     buttonClicked.parentElement.parentElement.remove()
     updateCartTotal()
 }
+function quantityChanged(event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+    }
+    updateCartTotal()
+}
+function addToCartClicked(event) {
+    var button = event.target
+    var shopItem = button.parentElement.parentElement
+    var title = shopItem.getElementsByClassName('shop-title')[0].innerText
+    var price = shopItem.getElementsByClassName('shop-price')[0].innerText
+    var image = shopItem.getElementsByClassName('shop-image')[0].src
+    console.log(title, price, image)
+    addToCartClicked(title, price, image)
+}
 
-function updateCartTotal(){
+function addToCart(title, price, image) {
+    var cartRow = document.createElement('tr')
+    cartRow.innerText = title
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    cartItems.apend(cartRow)
+
+}
+
+function updateCartTotal() {
     var cartItemContainer = document.getElementsByClassName('cart-items')[0]
     var cartRows = cartItemContainer.getElementsByClassName('cart-row')
     total = 0
-    for (var i = 0; i < cartRows.length; i++){
+    for (var i = 0; i < cartRows.length; i++) {
         cartRow = cartRows[i]
         var priceElement = cartRow.getElementsByClassName('cart-price')[0]
         var quantityElement = cartRow.getElementsByClassName('cart-quantity')[0]
-        var price = parseFloat(priceElement.innerText.replace ('$',''))
+        var price = parseFloat(priceElement.innerText.replace('$', ''))
         var quantity = quantityElement.value
         total = total + (price * quantity)
     }
+    total = '$' + Math.round(total * 100) / 100
     document.getElementsByClassName('subtotal')[0].innerText = total
     document.getElementsByClassName('sub-total')[0].innerText = total
 }
--- script.js
+++ script.js
@@ -1,41 +1,41 @@
 var mainImg = document.getElementById("big-Img");
 var smallImg = document.getElementsByClassName("small-img");
-const bar = document.getElementById("bar");
-const close = document.getElementById("close");
-const nav = document.getElementById("navbar");
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsById('navbar')
 
-toggleButton.addEventListener('click',() =>{
-    navbarLinks.classList.toggle('active')
+
+// smallImg[0].onclick = function(){
+//     mainImg.src = smallImg[0].src;
+// }
+// smallImg[1].onclick = function(){
+//     mainImg.src = smallImg[1].src;
+// }
+// smallImg[2].onclick = function(){
+//     mainImg.src = smallImg[2].src;
+// }
+// smallImg[3].onclick = function(){
+//     mainImg.src = smallImg[3].src;
+// }
+
+for (let i = 0; i < smallImg.length; i++) {
+   const eachSmallImgs = smallImg[i]
+    console.log(eachSmallImgs)
+
+    eachSmallImgs.addEventListener('click',(e)=>{
+        mainImg.src = smallImg[i].src
+    })
+
+}
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click",() =>{
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
 })
-
-
-
-smallImg[0].onclick = function(){
-    mainImg.src = smallImg[0].src;
-}
-smallImg[1].onclick = function(){
-    mainImg.src = smallImg[1].src;
-}
-smallImg[2].onclick = function(){
-    mainImg.src = smallImg[2].src;
-}
-smallImg[3].onclick = function(){
-    mainImg.src = smallImg[3].src;
-}
-
-if (bar){
-    bar.addEventListener('click',()=> {
-        nav.classList.add('active');
-    })
-}
-if (close){
-    close.addEventListener('click',()=> {
-        nav.classList.remove('active');
-    })
-}
-
+document.querySelectorAll(".nav-links").forEach(n => n.addEventListener("click", () =>{
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
 
 
 
